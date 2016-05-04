c9f4307aa8509372e205d2e9a18f453396e8d68f
PTony <jokr.01@gmail.com>
2016-05-04 12:02:27 +0200

    exemple requete

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -7,6 +7,9 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use AppBundle\Entity\Datauser;
 use AppBundle\Entity\User;
 use AppBundle\Entity\Link;
@@ -18,7 +21,12 @@ class UserController extends Controller
 {
 	public function showProfilAction (Request $request)
     {
-		$user = Doctrine::getTable('User')->findOneByUsername('test');
+    	$repository = $this->getDoctrine()
+    	 	->getRepository('AppBundle:User');
+    	$user = $repository->findByUsername("toto");
+
+    	var_dump($user);
+
         return $this->render('default/profil.html.twig', array('base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
     	));
     }
