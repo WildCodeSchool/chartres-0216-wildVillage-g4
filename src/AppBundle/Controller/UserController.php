46d8da0169e2c889b2e1df4c5ee84865d03ad89f
Gugusteh <augustin.lefebvre83@gmail.com>
2016-05-09 14:19:44 +0200

    changement profil ajout de liens

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -13,6 +13,7 @@ use AppBundle\Entity\Post;
 use AppBundle\Entity\User;
 use AppBundle\Entity\Link;
 use AppBundle\Entity\Message;
+use AppBundle\Entity\Profil_user;
 use DateTime;
     /**
      * @Route("/", name="profil")
@@ -29,30 +30,75 @@ class UserController extends Controller
         $datauser = $repository->findOneById_user($userid);
         echo "<br/> Username ";
         echo $user->getUsername();
+        echo "<br/>";
+        echo $datauser->getFirstname();
+        echo "<br/>";
+        echo $datauser->getSurname();
         echo "<br/> Email ";
         echo $user->getEmail();
         echo "<br/> Age ";
         echo $datauser->getAge();
+        
         return $this->render('default/profil.html.twig', array('base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
     }
     public function modifyAction (Request $request)
     {
         $em = $this->getDoctrine()->getManager();
+        
+        //declaration des variables
         $firstname = $request->request->get('firstname');
         $surname = $request->request->get('surname');
-
-
+        $age = $request->request->get('age');
+        $email = $request->request->get('email');
+        $mailispublic= $request->request->get('mailispublic');
+        $ageispublic = $request->request->get('ageispublic');
+        $surnameispublic = $request->request->get('surnameispublic');
+        $firstnameispublic = $request->request->get('firstnameispublic');
+        
+        //appel des tables
         $user = $this ->getUser();
-
         $repository = $em->getRepository('AppBundle:Datauser')->findOneById_user($user->getId());
+        $profil = $em->getRepository('AppBundle:Profil_user')->findOneById_user($user->getId());
 
-        $repository->setFirstname($firstname);
-        $repository->setSurname($surname);
+        //change les checkbox en booléens
+        $ageispublic = !empty($ageispublic) ? true : false ; 
+        $surnameispublic = !empty($surnameispublic) ? true : false ; 
+        $firstnameispublic = !empty($firstnameispublic) ? true : false ; 
+        $mailispublic = !empty($mailispublic) ? true : false ; 
+        
+
+        //verifie si les cases sont remplies pour modifier les infos
+        if(!empty($firstname))
+        {
+            $repository->setFirstname($firstname);
+        }
+        if(!empty($surname))
+        {
+            $repository->setSurname($surname);
+        }
+        if(!empty($email))
+        {
+            $user->setEmail($email);
+        }
+        if(!empty($age))
+        {        
+            $repository->setAge($age);
+        }
+
+        //remplit la table Profil_user
+        $profil->setIdUser($user->getId());
+        $profil->setBioIspublic(true);
+        $profil->setBio("coucou");
+        $profil->setMailIspublic($mailispublic);
+        $profil->setAgeIspublic($ageispublic);
+        $profil->setSurnameIspublic($surnameispublic);
+        $profil->setFirstnameIspublic($firstnameispublic);
 
         $em->persist($repository);
         $em->flush();
-        
+        $em->persist($profil);
+        $em->flush();
         return $this->render('default/profil.html.twig', array('base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
     }
